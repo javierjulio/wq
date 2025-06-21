84366a8ad30c5b27ce829c28c1f2354fb6b87a2e
Javier Julio <jjfutbol@gmail.com>
2025-06-21 19:12:48 -0400

    Split up spec to cover both ANSI and PlainText

--- spec/cli/colors_ext_spec.rb
+++ spec/cli/colors_ext_spec.rb
@@ -1,14 +1,32 @@
-RSpec.describe CommandKit::Colors::ANSI do
-  describe ".italics" do
-    context "when given a string" do
-      it "returns the string wrapped in italics ANSI codes" do
-        expect(described_class.italics("Hello")).to eq("\e[3mHello\e[23m")
+RSpec.describe CommandKit::Colors do
+  describe CommandKit::Colors::ANSI do
+    describe ".italics" do
+      context "when given a string" do
+        it "returns the string wrapped in italics ANSI codes" do
+          expect(described_class.italics("Hello")).to eq("\e[3mHello\e[23m")
+        end
+      end
+
+      context "when given no arguments" do
+        it "returns the italics ANSI code" do
+          expect(described_class.italics).to eq("\e[3m")
+        end
       end
     end
+  end
 
-    context "when given no arguments" do
-      it "returns the italics ANSI code" do
-        expect(described_class.italics).to eq("\e[3m")
+  describe CommandKit::Colors::PlainText do
+    describe ".italics" do
+      context "when given a string" do
+        it "returns the string as-is" do
+          expect(described_class.italics("Hello")).to eq("Hello")
+        end
+      end
+
+      context "when given no arguments" do
+        it "returns an empty string" do
+          expect(described_class.italics).to eq("")
+        end
       end
     end
   end
